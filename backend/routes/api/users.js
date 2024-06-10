4bd4bbd34eae25a6fba23e8d0f36014c0269c2e9
Jaspreet Singh <99jasingh@gmail.com>
2024-06-09 20:44:47 -0400

    Add validation

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,3 +1,5 @@
+const validateRegisterInput = require('../../validations/register');
+const validateLoginInput = require('../../validations/login');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
@@ -21,7 +23,7 @@ router.get('/current', restoreUser, (req, res) => {
 });
 
 // POST /api/users/register
-router.post('/register', async (req, res, next) => {
+router.post('/register', validateRegisterInput, async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
   const user = await User.findOne({
@@ -65,7 +67,7 @@ router.post('/register', async (req, res, next) => {
   });
 });
 
-router.post('/login', async (req, res, next) => {
+router.post('/login', validateLoginInput, async (req, res, next) => {
   passport.authenticate('local', async function(err, user) {
     if (err) return next(err);
     if (!user) {
