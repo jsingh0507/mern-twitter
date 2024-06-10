82e9468b70cb76166a99b46e37d2c0d1ab4ffdab
Jaspreet Singh <99jasingh@gmail.com>
2024-06-09 22:06:06 -0400

    Finish all phases

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import { AuthRoute } from './components/Routes/Routes';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
 import NavBar from './components/NavBar/NavBar';
 
 import MainPage from './components/MainPage/MainPage';
 import LoginForm from './components/SessionForms/LoginForm';
 import SignupForm from './components/SessionForms/SignupForm';
+import Tweets from './components/Tweets/Tweets';
+import Profile from './components/Profile/Profile';
+import TweetCompose from './components/Tweets/TweetCompose';
+
+import { getCurrentUser } from './store/session';
 
 const Layout = () => {
   return (
@@ -29,13 +38,37 @@ const router = createBrowserRouter([
       {
         path: "signup",
         element: <AuthRoute component={SignupForm} />
+      },
+      {
+        path: "tweets",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <ProtectedRoute component={Tweets} />
+          },
+          {
+            path: "new",
+            element: <ProtectedRoute component={TweetCompose} />
+          }
+        ]
+      },
+      {
+        path: "profile",
+        element: <ProtectedRoute component={Profile} />
       }
     ]
   }
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getCurrentUser()).finally(() => setLoaded(true));
+  }, [dispatch]);
+  
+  return loaded && <RouterProvider router={router} />;
 }
 
 export default App;
