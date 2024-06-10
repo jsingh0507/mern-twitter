40e7a45ecfb85aba01f6c5ddac63f371afa75bac
Jaspreet Singh <99jasingh@gmail.com>
2024-06-09 22:51:27 -0400

    Finish project

--- backend/app.js
+++ backend/app.js
@@ -66,5 +66,27 @@ app.use((req, res, next) => {
       errors: err.errors
     })
   });
+
+  if (isProduction) {
+    const path = require('path');
+    // Serve the frontend's index.html file at the root route
+    app.get('/', (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'dist', 'index.html')
+      );
+    });
+  
+    // Serve the static assets in the frontend's dist folder
+    app.use(express.static(path.resolve("../frontend/dist")));
+  
+    // Serve the frontend's index.html file at all other routes NOT starting with /api
+    app.get(/^(?!\/?api).*/, (req, res) => {
+      res.cookie('CSRF-TOKEN', req.csrfToken());
+      res.sendFile(
+        path.resolve(__dirname, '../frontend', 'dist', 'index.html')
+      );
+    });
+  }
   
 module.exports = app;
